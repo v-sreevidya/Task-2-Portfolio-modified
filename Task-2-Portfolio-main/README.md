aa94c4ea22209708ad94a588c9a81cc5f4fdf5f7
v-sreevidya <sreevidyavijayan14@gmail.com>
2024-07-01 18:05:58 +0530

    changes

---
+++ Task-2-Portfolio-main/README.md
@@ -0,0 +1,2 @@
+# Task-2-Portfolio
+Personal Portfolio web page that showcases my skills, projects, and provides a means for visitors to get in touch with me
---
+++ Task-2-Portfolio-main/script.js
@@ -0,0 +1,5 @@
+document.getElementById("contactForm").addEventListener("submit",function(event){
+    document.getElementById("name") .value = "";
+    document.getElementById("email").value = "";
+    document.getElementById("message").value = "";
+});
